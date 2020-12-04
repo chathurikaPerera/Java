fe9e89d7f9deecbead76be9fc96c8e19a556a88d
chathurikaPerera <pererachathurika96@gmail.com>
2020-12-04 22:51:56 +0530

    CRUD

---
+++ demo/src/main/java/com/example/demo/domain/Student.java
@@ -0,0 +1,68 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int Id;
+	private String username;
+	private String job;
+	private String email;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String username, String job, String email) {
+		super();
+		Id = id;
+		this.username = username;
+		this.job = job;
+		this.email = email;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+
+}
